5f30f76ef17c4ea33e089304661ee0932a98ce6b
rizkybaihaqy <rizkybaihaqy123@gmail.com>
2021-07-19 19:02:18 +0700

    add styling to pagination component

--- components/Pagination.js
+++ components/Pagination.js
@@ -13,27 +13,40 @@ export default function Pagination({
   }
 
   return (
-    <>
-      <label htmlFor="per-page">VIP Per Page</label>
-      <input
-        name="per-page"
-        type="number"
-        value={vipsPerPage}
-        onChange={(e) => {
-          if (e.target.value > 0) {
-            changeLimit(e.target.value);
-          }
-        }}
-      />
-      <nav>
-        <ul>
-          {pageNumbers.map((number) => (
-            <li key={number}>
-              <button onClick={() => paginate(number)}>{number}</button>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </>
+    <div className="flex justify-center">
+      <div>
+        <div className="flex items-center justify-center mb-5">
+          <label htmlFor="per-page" className="mr-2 font-bold">
+            VIP Per Page
+          </label>
+          <input
+            className="w-20 text-blue-800 font-bold"
+            name="per-page"
+            type="number"
+            value={vipsPerPage}
+            onChange={(e) => {
+              if (e.target.value > 0) {
+                changeLimit(e.target.value);
+              }
+            }}
+          />
+        </div>
+
+        <nav>
+          <ul className="flex justify-center">
+            {pageNumbers.map((number) => (
+              <li
+                key={number}
+                className="mx-1 px-3 py-2 bg-gray-200 text-gray-700 hover:bg-gray-700 hover:text-gray-200 rounded-lg"
+              >
+                <button onClick={() => paginate(number)} className="font-bold">
+                  {number}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </div>
   );
 }
